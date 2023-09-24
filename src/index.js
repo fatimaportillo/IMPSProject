f098f82823a6a2e0fdf99da408242c8f14e83ac8
fatima <fatimabeaportillo@gmail.com>
2023-09-23 21:40:34 -0600

    Conexion a base de datos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 //Inicializaciones
 const app = express();
 
+require('dotenv').config()
+
 //Ajustes del servidor
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4500);
+
+//Configuracion de rutas
+app.use(require('./routes'));//Node automaticamente busca el index.js del modulo
 
 // Iniciar el servidor
 app.listen(app.get('port'), () => {
